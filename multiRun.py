fb0b1c4d226b319e5d76ff42df30bad6a3215876
Edward Laird <elaird@gmail.com>
2013-01-22 13:32:17 +0100

    implement some options

--- multiRun.py
+++ multiRun.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 
-import os,analyze
+import os,optparse
+
+def opts() :
+    parser = optparse.OptionParser()
+    parser.add_option("--min", dest = "min", default = None, metavar = "N", help = "specify minimum run number")
+    parser.add_option("--max", dest = "max", default = None, metavar = "N", help = "specify maximum run number")
+    parser.add_option("--run", dest = "run", default = None, metavar = "N", help = "specify one run number")
+    options,args = parser.parse_args()
+    return options
 
 def rootFiles(directory = "", mode = "") :
     assert mode in ["usc","castor"],mode
@@ -27,7 +35,13 @@ directory = "/afs/cern.ch/user/e/elaird/work/public/d1_utca/"
 uscFiles    = rootFiles(directory, mode = "usc")
 castorFiles = rootFiles(directory, mode = "castor")
 
+options = opts()
+import analyze
+
 for run in sorted(uscFiles.keys()) :
+    if options.min and run<int(options.min) : continue
+    if options.max and run>int(options.max) : continue
+    if options.run and run!=int(options.run) : continue
     if run not in castorFiles : continue
     analyze.oneRun(utcaFileName = uscFiles[run],
                    cmsFileName = castorFiles[run] if (run in castorFiles) else "",
