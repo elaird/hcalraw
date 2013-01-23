2e485b5eb11a27d3dc6eed05247e8d7a794ceb65
Edward Laird <elaird@gmail.com>
2013-01-23 20:51:14 +0100

    re-use innerEvent in categorization

--- graphs.py
+++ graphs.py
@@ -1,23 +1,30 @@
 import ROOT as r
 
-def minutes(orn, bcn, evn = None) :
+def minutes(orn) :
     orbPerSec = 11.1e3
-    sec = (orn+bcn/3564.)/orbPerSec
-    return sec/60.0
+    return orn/orbPerSec/60.0
 
-def categories(outer = [], inner = []) :
+def categories(oMap = {}, iMap = {}, innerEvent = {}) :
     d = {}
-    for ornBcn in set(outer + inner) :
-        cat = 0
-        if ornBcn in inner : cat += 1
-        if ornBcn in outer : cat += 2
-        d[ornBcn] = cat
+    for oEvent,ornEvn in oMap.iteritems() :
+        orn = ornEvn[0]
+        if oEvent in innerEvent and innerEvent[oEvent]!=None :
+            d[orn] = 3
+        else :
+            d[orn] = 2
+
+    iEvents = innerEvent.values()
+    for iEvent,ornEvn in iMap.iteritems() :
+        if iEvent in iEvents : continue
+        orn = ornEvn[0]
+        d[orn] = 1
+
     return d
 
 def graph(d = {}) :
     gr = r.TGraph()
     for i,key in enumerate(sorted(d.keys())) :
-        gr.SetPoint(i, minutes(*key), d[key])
+        gr.SetPoint(i, minutes(key), d[key])
     return gr
 
 def labelAxis(h = None, labels = {}) :
@@ -28,14 +35,13 @@ def labelAxis(h = None, labels = {}) :
     yaxis.SetLabelSize(2.0*yaxis.GetLabelSize())
 
 def writePdf(label = "", oLabel = "", iLabel = "",
-             oMap = {}, iMap = {}, innerEvent = {}) :
-
-    outer = oMap.values()
-    inner = iMap.keys()
-    gr = graph(categories(outer = outer, inner = inner))
+             oMapF = {}, iMapF = {}, innerEvent = {}) :
+    cats = categories(oMap = oMapF, iMap = iMapF, innerEvent = innerEvent)
+    gr = graph(cats)
+    nBins = 30
     null = r.TH2D("null", "%s;time (minutes)"%label.replace("Run2", "Run 2"),
-                  1, 0.0, 1.1*minutes(*max(outer+inner)),
-                  3, 0.5, 3.5)
+                  1, 0.0, 4,#1.1*minutes(max(cats.keys())),
+                  nBins, 0.5, 0.5+nBins)
 
     canvas = r.TCanvas()
     canvas.SetLeftMargin(0.25)
@@ -44,8 +50,8 @@ def writePdf(label = "", oLabel = "", iLabel = "",
 
     null.Draw()
     nBoth = len(filter(lambda x:x!=None,innerEvent.values()))
-    labelAxis(null, labels = {1:"only %s (%d)"%(iLabel, len(iMap)-nBoth),
-                              2:"only %s (%d)"%(oLabel, len(oMap)-nBoth),
+    labelAxis(null, labels = {1:"only %s (%d)"%(iLabel, len(iMapF)-nBoth),
+                              2:"only %s (%d)"%(oLabel, len(oMapF)-nBoth),
                               3:"both (%d)"%nBoth})
 
     gr.SetMarkerStyle(20)
