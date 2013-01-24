a3edb36436767fc9a3f56cc111b2e0e967548d09
Edward Laird <elaird@gmail.com>
2013-01-24 11:19:54 +0100

    prettier

--- graphs.py
+++ graphs.py
@@ -7,13 +7,24 @@ def labelAxis(h = None, labels = {}) :
         yaxis.SetBinLabel(iBin, label)
     yaxis.SetLabelSize(2.0*yaxis.GetLabelSize())
 
+def xMax(graph = None, iPoint = 40) :
+    N = graph.GetN()
+    iPoint = min(iPoint, N-1)
+    x = r.Double()
+    y = r.Double()
+    graph.GetPoint(iPoint, x, y)
+    return float(x)
+
 def makeSummaryPdf(labels = [], pdf = "summary.pdf") :
     canvas = r.TCanvas()
     canvas.Print(pdf+"[")
 
+    pad0 = r.TPad("pad0", "pad0", 0.1, 0.9, 0.9, 1.0)
     pad1 = r.TPad("pad1", "pad1", 0.1, 0.7, 0.9, 0.9)
     pad2 = r.TPad("pad2", "pad2", 0.1, 0.1, 0.5, 0.5)
     pad3 = r.TPad("pad3", "pad3", 0.5, 0.1, 0.9, 0.5)
+
+    pad0.Draw()
     pad1.Draw()
     pad2.Draw()
     pad3.Draw()
@@ -21,22 +32,34 @@ def makeSummaryPdf(labels = [], pdf = "summary.pdf") :
     for label in labels :
         f = r.TFile("root/%s.root"%label)
 
+        #label
+        pad0.cd()
+        text = r.TText(0.5, 0.5, label.replace("Run2", "Run 2"))
+        text.SetNDC()
+        text.SetTextAlign(22)
+        text.SetTextSize(10.0*text.GetTextSize())
+        text.Draw()
+
         #category graph
         pad1.cd()
         r.gPad.SetLeftMargin(0.25)
+        r.gPad.SetBottomMargin(0.25)
         r.gPad.SetTickx()
         r.gPad.SetTicky()
 
-        nBins = 3
-        null = r.TH2D("null", "%s;time (minutes)"%label.replace("Run2", "Run 2"),
-                      1, 0.0, 4,#1.1*minutes(max(cats.keys())),
-                      nBins, 0.5, 0.5+nBins)
+        graph = f.Get("category_vs_time")
+        null = r.TH2D("null", ";time (minutes)", 1, 0.0, 1.1*xMax(graph), 3, 0.5, 3.5)
         null.Draw()
-        gr = f.Get("category_vs_time")
-        t = gr.GetTitle().split("_")
+        for axis in [null.GetXaxis(), null.GetYaxis()] :
+            axis.SetLabelSize(3.0*axis.GetLabelSize())
+            axis.SetTitleSize(3.0*axis.GetTitleSize())
+
+        t = graph.GetTitle().split("_")
         labelAxis(null, labels = {1:t[0], 2:t[1], 3:t[2]})
-        gr.SetMarkerStyle(20)
-        gr.Draw("psame")
+        graph.SetMarkerStyle(20)
+        graph.SetMarkerColor(r.gStyle.GetHistLineColor())
+        graph.SetMarkerSize(0.5*graph.GetMarkerSize())
+        graph.Draw("psame")
 
         pad2.cd()
         h2 = f.Get("deltaOrN")
