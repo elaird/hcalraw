312cc005360aedc8264ae017ac3db1e94aba9caa
Edward Laird <elaird@gmail.com>
2013-01-24 10:43:53 +0100

    multiple pads

--- graphs.py
+++ graphs.py
@@ -11,15 +11,23 @@ def makeSummaryPdf(labels = [], pdf = "summary.pdf") :
     canvas = r.TCanvas()
     canvas.Print(pdf+"[")
 
-    canvas.SetLeftMargin(0.25)
-    canvas.SetTickx()
-    canvas.SetTicky()
+    pad1 = r.TPad("pad1", "pad1", 0.1, 0.7, 0.9, 0.9)
+    pad2 = r.TPad("pad2", "pad2", 0.1, 0.1, 0.5, 0.5)
+    pad3 = r.TPad("pad3", "pad3", 0.5, 0.1, 0.9, 0.5)
+    pad1.Draw()
+    pad2.Draw()
+    pad3.Draw()
 
     for label in labels :
         f = r.TFile("root/%s.root"%label)
 
         #category graph
-        nBins = 30
+        pad1.cd()
+        r.gPad.SetLeftMargin(0.25)
+        r.gPad.SetTickx()
+        r.gPad.SetTicky()
+
+        nBins = 3
         null = r.TH2D("null", "%s;time (minutes)"%label.replace("Run2", "Run 2"),
                       1, 0.0, 4,#1.1*minutes(max(cats.keys())),
                       nBins, 0.5, 0.5+nBins)
@@ -29,6 +37,22 @@ def makeSummaryPdf(labels = [], pdf = "summary.pdf") :
         labelAxis(null, labels = {1:t[0], 2:t[1], 3:t[2]})
         gr.SetMarkerStyle(20)
         gr.Draw("psame")
+
+        pad2.cd()
+        h2 = f.Get("deltaOrN")
+        if h2 :
+            h2.SetStats(False)
+            h2.Draw("hist")
+            h2.SetMinimum(0.5)
+            r.gPad.SetLogy()
+
+        pad3.cd()
+        h3 = f.Get("deltaBcN")
+        if h3 :
+            h3.SetStats(False)
+            h3.Draw("hist")
+            h3.SetMinimum(0.5)
+            r.gPad.SetLogy()
 
         canvas.Print(pdf)
         f.Close()
