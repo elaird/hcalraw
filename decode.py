65ccc671b75f386ffeb4e1ee130a26000f2512e3
Edward Laird <elaird@gmail.com>
2013-01-27 20:37:16 +0100

    header supports either utca or vme

--- decode.py
+++ decode.py
@@ -1,4 +1,5 @@
-#see http://ohm.bu.edu/~hazen/CMS/SLHC/HcalUpgradeDataFormat_v1_2_2.pdf
+#For AMC13, see http://ohm.bu.edu/~hazen/CMS/SLHC/HcalUpgradeDataFormat_v1_2_2.pdf
+#For DCC2, see http://cmsdoc.cern.ch/cms/HCAL/document/CountingHouse/DCC/FormatGuide.pdf
 
 def bcn(raw, delta = 0) :
     if not delta : return raw
@@ -8,40 +9,47 @@ def bcn(raw, delta = 0) :
     return out
 
 def trailer(d = {}, iWord64 = None, word64 = None, bcnDelta = 0) :
-    d["TTS"] = (word64&0xf)>>2
-    d["CRC16"] = (word64&(0xffff<<16))>>16
-    d["nWord64"] = (word64&(0xffffff<<32))>>32
+    d["TTS"] = (word64>>4)&0xf
+    d["CRC16"] = (word64>>16)&0xffff
+    d["nWord64"] = (word64>>32)&0xffffff
 
-def header(d = {}, iWord64 = None, word64 = None, bcnDelta = 0) :
-    b = [((0xff<<8*i) & word64)>>8*i for i in range(8)]
+def htrDict(w) :
+    return {"nWord16":w&0x3ff,
+            "E":(w>>15)&1,
+            "P":(w>>14)&1,
+            "C":(w>>10)&1,
+            "V":not ((w>>12)&1 or (w>>13)&1),
+            }
 
+def uHtrDict(w) :
+    return {"nWord16":w&0xfff,
+            "E":(w>>15)&1,
+            "P":(w>>14)&1,
+            "V":(w>>13)&1,
+            "C":(w>>12)&1,
+            }
+
+def header(d = {}, iWord64 = None, word64 = None, utca = None, bcnDelta = 0) :
+    w = word64
     if iWord64==0 :
-        #d["eight"] = 0xf & b[0]
-        #d["fov"] = (0xf0 & b[0])/(1<<4)
-        d["FEDid"] = (0xf & b[2])*(1<<8) + b[1]
-        d["BcN"] = (0xf0 & b[2])/(1<<4) + b[3]*(1<<4)
-        d["EvN"] = b[4] + b[5]*(1<<8) + b[6]*(1<<16)
+        d["FEDid"] = (w>>8)&0xfff
+        d["BcN"] = (w>>20)&0xfff
+        d["EvN"] = (w>>32)&0xffffff
         d["BcN"] = bcn(d["BcN"], bcnDelta)
-        #d["evtTy"] = 0xf & b[7]
-        #d["five"] = (0xf0 & b[7])/(1<<4)
-
     if iWord64==1 :
-        #d["zero1"] = 0xf & b[0]
-        d["OrN"] = (0xf0 & b[0])/(1<<4) + b[1]*(1<<4) + b[2]*(1<<12) + b[3]*(1<<20) + (0xf & b[4])*(1<<28)
+        d["OrN"] = (w>>4)&0xffffffff
 
-    uhtr = {3:0, 4:4, 5:8}
-    if iWord64 in uhtr :
-        uhtr0 = uhtr[iWord64]
-        for i in range(4) :
-            key = "uHTR%d"%(uhtr0+i)
-            b0 = b[  2*i]
-            b1 = b[1+2*i]
-            d[key] = {"E":(b1&80)>>6,
-                      "P":(b1&40)>>5,
-                      "C":(b1&20)>>4,
-                      "V":(b1&10)>>3,
-                      "nWord16":(b1 & 0xf)*(1<<4) + b0,
-                      }
+    if utca :
+        if 3<=iWord64<=5 :
+            uhtr0 = 4*(iWord64-3)
+            for i in range(4) :
+                d["uHTR%d"%(uhtr0+i)] = uHtrDict((w>>(16*i))&0xffff)
+    else :
+        if 3<=iWord64<=10 :
+            j = (iWord64-3)*2
+            d["HTR%d"%j] = htrDict(w)
+            if iWord64!=10 :
+                d["HTR%d"%(j+1)] = htrDict(w>>32)
 
 def payload(d = {}, iWord16 = None, word16 = None, bcnDelta = 0) :
 #https://cms-docdb.cern.ch/cgi-bin/PublicDocDB/RetrieveFile?docid=3327&version=14&filename=HTR_MainFPGA.pdf
