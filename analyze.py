00020e20fd3bf6603c2a28ea775a1db07b3c6ee7
Edward Laird <elaird@gmail.com>
2013-05-03 14:23:40 +0200

    new run; separate raw blocks

--- analyze.py
+++ analyze.py
@@ -163,7 +163,9 @@ def unpacked(fedData=None, chars=None, skipHtrBlocks=False, skipTrailer=False,
         "Specify whether to unpack by words or chars."
     assert skipHtrBlocks or (utca in [False, True]), \
         "Specify whether data is uTCA or VME (unless skipping HTR blocks)."
-    d = {"htrBlocks": {}}
+    amc13Header = {}
+    amc13Trailer = {}
+    htrBlocks = {}
 
     nWord64 = fedData.size()/(8 if chars else 1)
     iWordPayload0 = 6 if utca else 12
@@ -186,19 +188,25 @@ def unpacked(fedData=None, chars=None, skipHtrBlocks=False, skipTrailer=False,
             word64 = fedData.at(iWord64)
 
         if iWord64 < iWordPayload0:
-            decode.header(d, iWord64, word64, utca, bcnDelta)
+            decode.header(amc13Header, iWord64, word64, utca, bcnDelta)
         elif iWord64 < nWord64-1:
             for i in range(4):
                 word16 = (word64 >> (16*i)) & 0xffff
-                decode.payload(d["htrBlocks"],
+                decode.payload(htrBlocks,
                                iWord16=4*iWord64+i, word16=word16,
-                               word16Counts=d["word16Counts"],
+                               word16Counts=amc13Header["word16Counts"],
                                utca=utca, bcnDelta=bcnDelta)
         else:
-            if "htrIndex" in d["htrBlocks"]:
-                del d["htrBlocks"]["htrIndex"]  # fixme
-            decode.trailer(d, iWord64, word64)
-    return d
+            if "htrIndex" in htrBlocks:
+                del htrBlocks["htrIndex"]  # fixme
+            decode.trailer(amc13Trailer, iWord64, word64)
+
+    # fixme: improve this
+    out = {}
+    out.update(amc13Header)
+    out.update(amc13Trailer)
+    out.update({"htrBlocks": htrBlocks})
+    return out
 
 
 def charsOneFed(tree=None, fedId=None, collection=""):
@@ -371,14 +379,15 @@ if __name__ == "__main__":
     #       ornTolerance=0,
     #       )
 
-    oneRun(utcaFileName=baseDir+"/usc/USC_211155.root",
-           cmsFileName=baseDir+"/usc/USC_211154.root",
-           cmsIsLocal=True,
-           label="Run211155",
-           useEvn=False,
-           filterEvn=False,
-           ornTolerance=0,
-           )
+    # 211155
+    #oneRun(utcaFileName=baseDir+"/usc/USC_211155.root",
+    #       cmsFileName=baseDir+"/usc/USC_211154.root",
+    #       cmsIsLocal=True,
+    #       label="Run211155",
+    #       useEvn=False,
+    #       filterEvn=False,
+    #       ornTolerance=0,
+    #       )
 
     #oneRun(utcaFileName=baseDir+"/usc/USC_211428.root",
     #       cmsFileName=baseDir+"/usc/USC_211427.root",
@@ -388,3 +397,12 @@ if __name__ == "__main__":
     #       filterEvn=False,
     #       ornTolerance=0,
     #       )
+
+    oneRun(utcaFileName=baseDir+"/904/B904_Integration_000000.uhtr.root",
+           #cmsFileName=baseDir+"/904/B904_Integration_000000.uhtr.root",
+           cmsIsLocal=True,
+           label="Run0",
+           useEvn=False,
+           filterEvn=False,
+           ornTolerance=0,
+           )
