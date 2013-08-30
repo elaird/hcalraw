8ab21c0aaddd8353bd3ac60f6298b496b50f9e34
Edward Laird <elaird@gmail.com>
2013-08-30 16:30:55 +0200

    pass as option

--- configuration.py
+++ configuration.py
@@ -21,7 +21,6 @@ def utca(uhtr=False):
                           "ErrF": [3],
                           },
             "unpackSkipFlavors": [4],
-            "patternMode": False,
             "matchRange": {930: range(10),    # B904
                            931: range(10),    # B904
                            989: range(10),    # Jan. slice-test
@@ -44,7 +43,6 @@ def cms(local=False):
            "unpackSkipFlavors": [7],
            "printSkip": {"fibCh": [0, 2],
                          "ErrF": [3]},
-           "patternMode": False,
            }
     if local:
         out.update({"treeName": "CMSRAW",
