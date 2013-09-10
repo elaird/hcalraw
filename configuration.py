c6b3c6f1d421b06f9a83095db01bb46803a53bcb
Edward Laird <elaird@gmail.com>
2013-09-10 16:52:11 +0200

    restructure; detect file type from tree name

--- configuration.py
+++ configuration.py
@@ -1,64 +1,92 @@
-def utca(uhtr=False):
-    d2c = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6,
-           7:   9,
-           8:  11,
-           9:  12,
-           10: 10,
-           11:  8,
-           12:  7,
-           }
+def nPatternFibers():
+    return 6
 
-    return {"label": "uTCA",
-            "treeName": "CMSRAW",
-            "format": "HCAL",
-            "auxBranch": False,
-            "utca": True,
-            "branchName": "Chunk",
-            "bcnDelta": -118,
-            "fiberMap": {} if uhtr else d2c,
-            "nEventsMax": None,
-            "printSkip": {"fibCh": [0, 2],
-                          "ErrF": [3],
-                          },
-            "unpackSkipFlavors": [4],
-            "matchRange": {930: range(10),    # B904
-                           931: range(10),    # B904
-                           989: range(10),    # Jan. slice-test
-                           990: range(1, 10), # Jan. slice-test (HF)
-                           },
-            "nPatternFibers": 6,
-            "nPatternTs": 10,
+
+def nPatternTs():
+    return 10
+
+
+def printSkip():
+    return {"fibCh": [0, 2],
+            "ErrF": [3],
             }
 
 
-def cms(local=False):
-    matchRange = {}
-    for iFed in range(700, 732):
-        matchRange[iFed] = range(10)
-    matchRange[722] = range(9) # Jan. slice-test
-
-    out = {"label": "CMS",
-           "utca": False,
-           "matchRange": matchRange,
-           "bcnDelta": 0,
-           "fiberMap": {},
-           "nEventsMax": None,
-           "unpackSkipFlavors": [7],
-           "printSkip": {"fibCh": [0, 2],
-                         "ErrF": [3]},
-           "nPatternFibers": 6,
-           "nPatternTs": 10,
-           }
-    if local:
-        out.update({"treeName": "CMSRAW",
-                    "format":  "HCAL",
-                    "auxBranch": False,
-                    "branchName": "HCAL_DCC",
-                    })
+def unpackSkipFlavors(fedId=None):
+    if isVme(fedId):
+        return [7]
     else:
-        out.update({"treeName": "Events",
-                    "format":  "CMS",
-                    "auxBranch": True,
-                    "rawCollection": "FEDRawDataCollection_rawDataCollector__LHC",
-                    })
+        return [4]
+
+
+def bcnDelta(fedId=None):
+    return 0 if isVme(fedId) else -118
+
+
+def matchRange(fedId=None):
+    # exceptions for Jan. 2013 slice-test (HF)
+    if fedId == 990:
+        return range(1, 10)
+    elif fedId == 722:
+        return range(9)
+    else:
+        return range(10)
+
+
+def useEvn():
+    return False
+
+def filterEvn():
+    return False
+
+def ornTolerance():
+    return 0
+
+def printEventMap():
+    return False
+
+def identityMap():
+    return False
+
+def fiberMap():
+    #return d2c()
+    return {}
+
+
+def isVme(fedId=None):
+    return 700 <= fedId <= 731
+
+
+def format(treeName="", someFedId=None):
+    assert someFedId
+    out = None
+    if treeName == "CMSRAW":
+        out = {"name":  "HCAL",
+               "branch": "HCAL_DCC" if isVme(someFedId) else "Chunk",
+               "auxBranch": False,
+               }
+
+    if treeName == "moltree":
+        out = {"name": "MOL",
+               "branch": "vec",
+               }
+
+    if treeName == "Events":
+        out = {"name": "CMS",
+               "auxBranch": True,
+               "rawCollection": "FEDRawDataCollection_rawDataCollector__LHC",
+               }
+    if out:
+        out["treeName"] = treeName
     return out
+
+
+def d2c():
+    return {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6,
+            7:   9,
+            8:  11,
+            9:  12,
+            10: 10,
+            11:  8,
+            12:  7,
+            }
