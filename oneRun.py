0702c2071f234742ad11d8e0560e650dc81ccce7
Edward Laird <elaird@gmail.com>
2013-09-11 10:02:29 +0200

    pass fed lists to analyze.oneRun; define functions

--- oneRun.py
+++ oneRun.py
@@ -5,9 +5,9 @@ def opts():
     import optparse
     parser = optparse.OptionParser()
     parser.add_option("--file1", dest="file1", default="", help="REQUIRED: .root file over which to run")
-    parser.add_option("--feds1", dest="feds1", default="", help="REQUIRED: FEDs to use in file1")
+    parser.add_option("--feds1", dest="feds1", default="", help="REQUIRED: FEDs to use in file1, e.g. 714,722")
     parser.add_option("--file2", dest="file2", default="", help=".root file to compare with file1")
-    parser.add_option("--feds2", dest="feds2", default="", help="FEDs to use in file2")
+    parser.add_option("--feds2", dest="feds2", default="", help="FEDs to use in file2, e.g. 931")
     parser.add_option("--patterns", dest="patterns", default=False, action="store_true", help="interpret QIE data as FE patterns")
     parser.add_option("--nevents", dest="nevents", default="", metavar="N", help="stop after N events")
 
@@ -19,30 +19,54 @@ def opts():
     return options
 
 
-options = opts()
-if not all([x[0] == "_" or x in ["x", "opts", "options"] for x in dir()]):
-    print dir()
-    print "Please put imports after this block, to prevent PyROOT from stealing '--help'."
+def nEvents(s=""):
+    if s:
+        try:
+            return int(s)
+        except ValueError:
+            print "ERROR: nevents '%s' cannot be converted to an int." % s
+            exit()
+    else:
+        return None
 
 
-if options.nevents:
-    try:
-        nEvents = int(options.nevents)
-    except ValueError:
-        print "ERROR: nevents '%s' cannot be converted to an int." % options.nevents
+def checkFeds(feds=[]):
+    import configuration
+    if len(set([configuration.isVme(fed) for fed in feds])) != 1:
+        print "ERROR: fed list %s is mixed among uTCA and VME." % str(l)
         exit()
-else:
-    nEvents = None
 
+
+def fedList(s=""):
+    if not s:
+        return []
+    out = [int(x) for x in s.split(",")]
+    checkFeds(out)
+    return out
+
+
+def checkModules():
+    import types
+    names = []
+    for key, value in globals().iteritems():
+        if isinstance(value, types.ModuleType):
+            names.append(key)
+    if names != ["__builtins__"]:
+        print names
+        print "Please put imports after this block, to prevent PyROOT from stealing '--help'."
+
+
+options = opts()
+checkModules()
 
 import analyze
 
 label = "latest"
 analyze.oneRun(file1=options.file1,
-               feds1=options.feds1,
+               feds1=fedList(options.feds1),
                file2=options.file2,
-               feds2=options.feds2,
-               nEvents=nEvents,
+               feds2=fedList(options.feds2),
+               nEvents=nEvents(options.nevents),
                patternMode=options.patterns,
                label=label,
                )
