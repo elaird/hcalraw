9004e601065a94983242ac1b25167777a14ddce4
Edward Laird <elaird@gmail.com>
2014-02-26 17:52:27 +0100

    rename; colors

--- printer.py
+++ printer.py
@@ -1,25 +1,41 @@
+# https://wiki.archlinux.org/index.php/Color_Bash_Prompt
+
 __color = True  # overwritten by oneRun.py
 
-
-def msg(s="", n=0):
+def __line(f='', s=''):
     if __color:
-        print ('\033[%dm' % n) + s + '\033[0m'
+        print '\033['+ f + s + '\033[0m'
     else:
         print s
 
-def warning(s=""):
-    msg("WARNING: "+s, 93)
 
-def error(s=""):
-    msg("ERROR: "+s, 91)
-
-def info(s=""):
-    msg("INFO: "+s, 94)
-        
-def more(s=""):
-    msg(s, 94)
-
-def less(s=""):
-    msg(s, 92)
+def error(s=''):
+    __line('91m', 'ERROR: '+s)
 
 
+def warning(s=''):
+    __line('93m','WARNING: '+s)
+
+
+def msg(s=''):
+    __line('30m', s)
+
+
+def green(s=''):
+    __line('32m', s)
+
+
+def yellow(s=''):
+    __line('33m', s)
+
+
+def blue(s=''):
+    __line('34m', s)
+
+
+def purple(s=''):
+    __line('35m', s)
+
+
+def cyan(s=''):
+    __line('36m', s)
