480f0129a76b5675763633bce57182c5251ac0a1
Edward Laird <elaird@gmail.com>
2014-02-28 16:50:56 +0100

    group options; pep8

--- oneRun.py
+++ oneRun.py
@@ -4,13 +4,39 @@
 def opts():
     import optparse
     parser = optparse.OptionParser()
-    parser.add_option("--file1", dest="file1", default="", help="REQUIRED: .root file over which to run")
-    parser.add_option("--feds1", dest="feds1", default="", help="REQUIRED: FEDs to use in file1, e.g. 714,722 or e.g. HBHE")
-    parser.add_option("--file2", dest="file2", default="", help=".root file to compare with file1")
-    parser.add_option("--feds2", dest="feds2", default="", help="FEDs to use in file2, e.g. 931")
-    parser.add_option("--patterns", dest="patterns", default=False, action="store_true", help="interpret QIE data as FE patterns")
-    parser.add_option("--nevents", dest="nevents", default="", metavar="N", help="stop after N events")
-    dump = ["dump level (0-6)",
+
+    reqd = optparse.OptionGroup(parser, "REQUIRED")
+    reqd.add_option("--file1",
+                    dest="file1",
+                    default="",
+                    help=".root file over which to run")
+    reqd.add_option("--feds1",
+                    dest="feds1",
+                    default="",
+                    help="FEDs to use in file1, e.g. 714,722 or e.g. HBHE")
+    parser.add_option_group(reqd)
+
+    common = optparse.OptionGroup(parser, "Optional")
+    common.add_option("--file2",
+                      dest="file2",
+                      default="",
+                      help=".root file to compare with file1")
+    common.add_option("--feds2",
+                      dest="feds2",
+                      default="",
+                      help="FEDs to use in file2, e.g. 931")
+    common.add_option("--patterns",
+                      dest="patterns",
+                      default=False,
+                      action="store_true",
+                      help="interpret QIE data as FE patterns")
+    common.add_option("--nevents",
+                      dest="nevents",
+                      default="",
+                      metavar="N",
+                      help="stop after N events")
+
+    dump = ["dump level (0-6), default is 0.",
             "0: only summary (no per-event info)",
             "1: DCC/AMC13 headers",
             "2: (u)HTR summary info",
@@ -19,9 +45,17 @@ def opts():
             "5: data (fibers with ErrF != 3); tps (> 0)",
             "6: data (all); tps (all)",
             ]
-    parser.add_option("--dump", dest="dump", default=0, metavar="D",
+    common.add_option("--dump",
+                      dest="dump",
+                      default=0,
+                      metavar="D",
                       help=" ".join([d.ljust(60) for d in dump]))
-    parser.add_option("--no-color", dest="noColor", default=False, action="store_true", help="disable color in stdout")
+    common.add_option("--no-color",
+                      dest="noColor",
+                      default=False,
+                      action="store_true",
+                      help="disable color in stdout")
+    parser.add_option_group(common)
 
     match = optparse.OptionGroup(parser, "Options for matching events across files")
     match.add_option("--use-evn",
