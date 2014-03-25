cdb94643924de558ac3371ca91535ba06354ba78
Edward Laird <elaird@gmail.com>
2014-03-25 11:37:22 +0100

    print in table format

--- diff.py
+++ diff.py
@@ -21,6 +21,7 @@ def mapping(file=None, skip=[]):
 def diffs(ref={}, cabled={}):
     missing = {}
     different = {}
+    same = {}
     for be, fe in sorted(ref.iteritems()):
         if be not in cabled:
             missing[be] = fe
@@ -28,8 +29,9 @@ def diffs(ref={}, cabled={}):
 
         if fe != cabled[be]:
             different[be] = (fe, cabled[be])
-
-    return missing, different
+        else:
+            same[be] = fe
+    return missing, different, same
 
 
 def pretty(be=None, fe=None):
@@ -41,7 +43,65 @@ def pretty(be=None, fe=None):
     return s
 
 
-def report(missing=None, different=None):
+def fiberCount(feCoords=[]):
+    out = {}
+    for coords in feCoords:
+        rbx = coords[0]
+        if rbx in out:
+            out[rbx] += 1
+        else:
+            out[rbx] = 1
+    return out
+
+
+def printTable(rbxes={}, header="", zero="  "):
+    if header:
+        header = "| %s |" % header
+        print "-"*len(header)
+        print header
+        print "-"*len(header)
+
+    boxes = ["%02d" % i for i in range(1, 19)]
+    dets = ["HBM", "HBP",
+            "HEM", "HEP",
+            "HFM", "HFP",
+            "HO2M", "HO1M", "HO0", "HO1P", "HO2P"]
+    nSpaces = 5
+    header = " ".join([" "*nSpaces] + boxes)
+    print header
+    print "-" * len(header)
+    for det in dets:
+        row = []
+        for box in boxes:
+            key = det + box
+            nFibers = rbxes.get(key)
+            if nFibers is None:
+                row.append(zero)
+            else:
+                row.append("%2d" % nFibers)
+        print " ".join([det.ljust(nSpaces)] + row)
+    print
+
+
+def report(missing=None, different=None, same=None):
+    printTable(fiberCount(same.values()), header="Fibers matching reference")
+    nMissing = fiberCount(missing.values())
+    printTable(nMissing, header="Missing Fibers")
+
+    header = "| RBXes missing exactly one fiber |"
+    print "-"*len(header)
+    print header
+    print "-"*len(header)
+    print "DCC SP FI: ref. RBX RM FI"
+    if not nMissing.values().count(1):
+        print "None"
+
+    for be, ref in sorted(missing.iteritems()):
+        rbx = ref[0]
+        if nMissing[rbx] == 1:
+            print pretty(be=be, fe=ref)
+
+    print
     print "---------------"
     print "| Differences |"
     print "---------------"
@@ -52,42 +112,12 @@ def report(missing=None, different=None):
     else:
         print "None"
 
-    if missing:
-        rbxes = [x[0] for x in missing.values()]
-        header = "| RBXes missing exactly one fiber |"
-        print
-        print "-"*len(header)
-        print header
-        print "-"*len(header)
-        print "DCC SP FI: ref. RBX RM FI"
-
-        nSingle = 0
-        for be, ref in sorted(missing.iteritems()):
-            rbx = ref[0]
-            if rbxes.count(rbx) == 1:
-                print pretty(be=be, fe=ref)
-                rbxes.remove(rbx)
-                nSingle += 1
-        if not nSingle:
-            print "None"
-
-        header = "| RBXes missing at least two fibers |"
-        print
-        print "-"*len(header)
-        print header
-        print "-"*len(header)
-        if rbxes:
-            for rbx in sorted(set(rbxes)):
-                print "%6s (%2d fibers)" % (rbx, rbxes.count(rbx))
-        else:
-            print "None"
-
 
 if __name__ == "__main__":
     with open("data/ref.txt") as f:
         ref, refMisc = mapping(f)
 
-    cabled, misc = mapping(fileinput.input(), skip=["Xrd", "TClassTable"])
+    cabled, misc = mapping(fileinput.input(), skip=["Xrd", "TClassTable", "nologin"])
 
     assert not refMisc
     for item in sorted(misc):
