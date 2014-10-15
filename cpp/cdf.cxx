d39efd461de7508323f5358dd97d156b586a0e9b
Edward Laird <elaird@gmail.com>
2014-10-15 11:44:18 +0200

    reduce to a function

--- cpp/cdf.cxx
+++ cpp/cdf.cxx
@@ -1,16 +1,6 @@
-//linkdef for reading Tree
 #include "CDFROOTLinkDef.h"
 
-//expose std::vector
-class CDFChunk2 : public CDFChunk {
-public:
-  CDFChunk2(const CDFChunk& in) {
-    fChunkLength = in.fChunkLength;
-    fChunk = in.fChunk;
-  }
-  Int_t length() {return fChunkLength;}
-  std::vector<ULong64_t> chunk() {
-    return std::vector<ULong64_t> (fChunk, fChunk + fChunkLength);
-  }
-  ClassDef(CDFChunk2, 1);
-};
+std::vector<ULong64_t> CDFChunk2(CDFChunk& in) {
+  ULong64_t* data = in.getData();
+  return std::vector<ULong64_t> (data, data + in.getDataLength());
+}
