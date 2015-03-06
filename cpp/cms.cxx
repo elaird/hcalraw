47834b53dbb0aa1836ed30a4c467cc20cd4937f1
Edward Laird <elaird@gmail.com>
2015-03-06 11:37:55 +0100

    rename existing function; add another

--- cpp/cms.cxx
+++ cpp/cms.cxx
@@ -1,6 +1,16 @@
 #include "FEDRawData.cc"
+#include <stdint.h>
 
-const std::vector<unsigned char> FEDRawData2(const FEDRawData& in) {
+const std::vector<unsigned char> FEDRawDataChars(const FEDRawData& in) {
   const unsigned char * data = in.data();
   return std::vector<unsigned char> (data, data + in.size());
+}
+
+const std::vector<uint64_t> FEDRawDataWords(const FEDRawData& in) {
+  if (in.size() & 0x7) {
+    std::cerr << "ERROR: payload truncated" << std::endl;
+  }
+  int s = in.size()/8;
+  uint64_t * data = (uint64_t *) in.data();
+  return std::vector<uint64_t> (data, data + s);
 }
