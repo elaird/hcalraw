ea82986a5a1d9c2b02e4cf7aa12b119879b18fd5
Edward Laird <elaird@gmail.com>
2015-03-24 17:33:08 +0100

    test forward transformation

--- test_transformation.py
+++ test_transformation.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
 
-from configuration import transformed_qie
+from configuration import transformed_qie, transformed_tp
 
 
 def check(l):
     s = set(l)
     if len(l) != len(s):
-        print len(l) - len(s)
+        print "ERROR: %d collisions." % (len(l) - len(s))
+        for item in sorted(l):
+            print item
 
 
-def one(crate=None, fibCh=None, slots=[], tops="", fibers=[]):
+def qie(crate=None, fibCh=None, slots=[], tops="", fibers=[]):
     l = []
     for slot in slots:
         for top in tops:
@@ -24,9 +26,36 @@ def one(crate=None, fibCh=None, slots=[], tops="", fibers=[]):
     check(l)
 
 
+def tp_vme_hf(crate=2, slb=6):
+    l = []
+
+    for slot in [2, 4, 6] + [13, 15, 17]:
+        for top, chs in {"t": [2, 3], "b": [6, 7]}.iteritems():
+            for ch in chs:
+                key = (slb, ch)
+                c1 = (crate, slot, top, key)
+                c2 = transformed_tp(*c1)
+                # c3 = transformed_tp(*c2)
+                # if c3 != c1:
+                #     print c1, c2, c3
+                l.append(c2)
+
+    for slot in [3, 5, 7] + [14, 16, 18]:
+        for top, chs in {"t": [0, 1], "b": [4, 5]}.iteritems():
+            for ch in chs:
+                key = (slb, ch)
+                c1 = (crate, slot, top, key)
+                c2 = transformed_tp(*c1)
+                # c3 = transformed_tp(*c2)
+                # if c3 != c1:
+                #     print c1, c2, c3
+                l.append(c2)
+    check(l)
+
+
 if __name__ == "__main__":
     # VME
-    one(crate=0,
+    qie(crate=0,
         fibCh=1,
         slots=range(2, 8) + range(13, 19),
         tops="tb",
@@ -34,9 +63,11 @@ if __name__ == "__main__":
     )
 
     # uTCA
-    one(crate=20,
+    qie(crate=20,
         fibCh=1,
         slots=range(1, 13),
         tops=" ",
         fibers=range(2, 10) + range(14, 22),
     )
+
+    tp_vme_hf()
