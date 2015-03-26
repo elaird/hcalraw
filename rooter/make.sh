49783d0c556ee64d1c4a8c90c8a5741f3ca642a5
Edward Laird <elaird@gmail.com>
2015-03-26 18:19:05 +0100

    add file

--- rooter/make.sh
+++ rooter/make.sh
@@ -2,3 +2,4 @@
 
 g++ rooter.cpp -I `root-config --incdir --libs` -o rooter
 g++ deadbeef.cpp -I `root-config --incdir --libs` -o deadbeef
+g++ badcoffee.cpp -I `root-config --incdir --libs` -o badcoffee
