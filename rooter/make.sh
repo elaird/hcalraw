d86936daafe8a457c3660be4cef6896862e1fb21
Edward Laird <elaird@gmail.com>
2015-03-26 18:29:15 +0100

    move files

--- rooter/make.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-g++ rooter.cpp -I `root-config --incdir --libs` -o rooter
-g++ deadbeef.cpp -I `root-config --incdir --libs` -o deadbeef
-g++ badcoffee.cpp -I `root-config --incdir --libs` -o badcoffee
