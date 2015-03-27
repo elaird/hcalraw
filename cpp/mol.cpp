31086ca0ec2dd13fe96328a23682258824d0e4c1
Edward Laird <elaird@gmail.com>
2015-03-27 16:30:38 +0100

    init

---
+++ cpp/mol.cpp
@@ -0,0 +1,11 @@
+#define START_FLAG (1L<<39) // initial block: bit 31 --> 39 (endian flip), as well as zeroes in 42..32
+#define START_MASK 0x80ffff0000L
+#define SAVE_HEADER 1
+
+#define WORD uint64_t
+#define NHEADER 2
+#define MAGIC 0x5a47 // GZ
+#define MASK  0xffff
+#define TAG "mol"
+
+#include "mb_loop.cpp"
