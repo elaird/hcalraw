cedc2c31a36328f902dc137a31468b6c88bdc061
Edward Laird <elaird@gmail.com>
2015-03-27 09:42:29 +0100

    swap defines

--- cpp/badcoffee.cpp
+++ cpp/badcoffee.cpp
@@ -1,6 +1,6 @@
-#define MAGIC 0xbadc0ffeebadcafe
-#define NHEADER 2  // MAGIC, nWords64
 #define WORD uint64_t
+#define NHEADER 2
+#define MAGIC 0xbadc0ffeebadcafe
 #define TAG "badcoffee"
 
 #include "mb_loop.cpp"
