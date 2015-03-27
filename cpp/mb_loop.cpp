0189f589272b90fe63ea7950ae5d2aa81b7378a6
Edward Laird <elaird@gmail.com>
2015-03-27 10:04:08 +0100

    use mask; optionally save header

--- cpp/mb_loop.cpp
+++ cpp/mb_loop.cpp
@@ -6,6 +6,15 @@
 #include <iostream>
 #include <stdint.h>
 
+#define SAVE_HEADER 0  // switch to store the header word into the root file
+
+
+int NDataInBlock(WORD buf){
+  int nData = (buf>>56);
+  nData += (buf>>40) & 0x300;
+  return nData;
+}
+
 
 int main(int argc, char* argv[]) {
   gROOT->ProcessLine("#include <vector>; #pragma link C++ class vector<uint32_t>+; #pragma link C++ class vector<uint64_t>+;");
@@ -21,20 +30,21 @@ int main(int argc, char* argv[]) {
   TTree tree(TAG,"");
   
 
-  int iWordInBlock = 0;
   std::vector<WORD> vec(1 << 13);
   vec.clear();
 
-  tree.Branch(Form("bc%s", argv[1]), &vec);
+  tree.Branch(Form("%s", argv[1]), &vec);
+
+  int iWordInBlock = 0;
+  int nWordsInBlock = 0;
 
   WORD buf;
-  int nWordsInBlock = 0;
   int success = fread(&buf, sizeof(buf), 1, stdin);  // read a word!
 
   while (success) {
     if (debug) std::cout << iWordInBlock;
 
-    if (buf == MAGIC) {
+    if ((buf & MASK) == MAGIC) {
       if((iWordInBlock - NHEADER != nWordsInBlock) && nWordsInBlock)
         std::cout << "Warning::Found magic number in data with iWords = " << iWordInBlock << " nWordsInBlock = " << nWordsInBlock << std::endl;
       else if (iWordInBlock){
@@ -50,7 +60,7 @@ int main(int argc, char* argv[]) {
       }
     }
 
-    if(NHEADER <= iWordInBlock){  //choose to store the block header
+    if(SAVE_HEADER || (NHEADER <= iWordInBlock)){
       if(debug) std::cout << " pushing_back " << std::hex << buf << std::dec << std::endl;
       vec.push_back(buf);  // store the word into vector
     }
