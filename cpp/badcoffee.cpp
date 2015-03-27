eac395dfe56f785500863785c081aded5389765d
Edward Laird <elaird@gmail.com>
2015-03-27 10:03:38 +0100

    add mask

--- cpp/badcoffee.cpp
+++ cpp/badcoffee.cpp
@@ -1,6 +1,7 @@
 #define WORD uint64_t
 #define NHEADER 2
 #define MAGIC 0xbadc0ffeebadcafe
+#define MASK  0xffffffffffffffff
 #define TAG "badcoffee"
 
 #include "mb_loop.cpp"
--- cpp/deadbeef.cpp
+++ cpp/deadbeef.cpp
@@ -1,6 +1,7 @@
 #define WORD uint32_t
 #define NHEADER 2
 #define MAGIC 0xdeadbeef
+#define MASK  0xffffffff
 #define TAG "deadbeef"
 
 #include "mb_loop.cpp"
