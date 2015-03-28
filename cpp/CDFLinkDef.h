92cd4b797be8dcc3e8c2433094ce56f62d5b6baf
Edward Laird <elaird@gmail.com>
2015-03-28 09:46:33 +0100

    split linkdef

---
+++ cpp/CDFLinkDef.h
@@ -0,0 +1,11 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class CDFChunk+;
+#pragma link C++ class CDFEventInfo+;
+#pragma link C++ function CDFChunk2;
+
+#endif
--- cpp/CMSLinkDef.h
+++ cpp/CMSLinkDef.h
@@ -1,19 +1,8 @@
-#include "CDFChunk.h"
-#include "CDFEventInfo.h"
-#include "cdf.h"
-
-#include "FEDRawDataCollection.h"
-#include "cms.h"
-
 #ifdef __CINT__
 
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
-
-#pragma link C++ class CDFChunk+;
-#pragma link C++ class CDFEventInfo+;
-#pragma link C++ function CDFChunk2;
 
 #pragma link C++ class FEDRawData+;
 #pragma link C++ class FEDRawDataCollection+;
