292f2362550dab0c19779c8b520337cf881e7669
Edward Laird <elaird@gmail.com>
2015-03-29 16:30:14 +0200

    tweak

--- oneRun.py
+++ oneRun.py
@@ -26,16 +26,15 @@ def main(options):
     if options.noColor:
         printer.__color = False
 
-    printOptions = subset(options, ["dump", "progress"])
-    printOptions["warn"] = not options.noWarnUnpack
-    printOptions["crateslots"] = configuration.fedList(options.crateslots)
-
     kargs = subset(options, ["feds1", "feds2", "unpackSkipFlavors"], process=True)
     kargs.update(subset(options, ["file1", "file2", "nEvents", "nEventsSkip", "outputFile", "noUnpack"]))
-    kargs["printOptions"] = printOptions
+
     kargs["patternMode"] = subset(options, ["rmRibbon", "nTs", "patternB"]) if options.patterns else None
     kargs["compareOptions"] = subset(options, ["adcPlots", "anyEmap", "printEmap"])
     kargs["mapOptions"] = subset(options, ["filterEvn", "printEventMap", "identityMap"])
+    kargs["printOptions"] = subset(options, ["dump", "progress"])
+    kargs["printOptions"]["warn"] = not options.noWarnUnpack
+    kargs["printOptions"]["crateslots"] = configuration.fedList(options.crateslots)
 
     def go():
         analyze.oneRun(**kargs)
