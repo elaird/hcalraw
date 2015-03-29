8fe48e11e78d43be58b3a11352071f1bd4ac0739
Edward Laird <elaird@gmail.com>
2015-03-29 17:52:06 +0200

    shuffle env

--- env/lxplus6.sh
+++ env/lxplus6.sh
@@ -1,2 +1,2 @@
 source env/slc6-cmssw.sh
-source env/slc6-eos.sh
+source env/eos.sh
--- env/slc6-cmssw.sh
+++ env/slc6-cmssw.sh
@@ -2,3 +2,4 @@ export SCRAM_ARCH=slc6_amd64_gcc472
 cd /afs/cern.ch/cms/slc6_amd64_gcc472/cms/cmssw/CMSSW_5_3_16/src
 eval `/afs/cern.ch/cms/common/scramv1 runtime -sh`
 cd - > /dev/null
+source /afs/cern.ch/project/eos/installation/cms/etc/setup.sh
