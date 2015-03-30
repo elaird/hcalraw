e8cbebcd0e2ad03cfe12d2b5fe07de8a96d305ea
Edward Laird <elaird@gmail.com>
2015-03-30 10:09:56 +0200

    update cmssw

--- env/slc6-cmssw.sh
+++ env/slc6-cmssw.sh
@@ -1,5 +1,5 @@
-export SCRAM_ARCH=slc6_amd64_gcc472
-cd /afs/cern.ch/cms/slc6_amd64_gcc472/cms/cmssw/CMSSW_5_3_16/src
+export SCRAM_ARCH=slc6_amd64_gcc481
+cd /afs/cern.ch/cms/${SCRAM_ARCH}/cms/cmssw/CMSSW_7_3_5/src
 eval `/afs/cern.ch/cms/common/scramv1 runtime -sh`
 cd - > /dev/null
 source /afs/cern.ch/project/eos/installation/cms/etc/setup.sh
