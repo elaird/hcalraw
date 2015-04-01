790f920b49a3e7a61994081ee48bcc4560c6dc90
Edward Laird <elaird@gmail.com>
2015-04-01 05:37:51 +0200

    do not histogram pad words

--- compare.py
+++ compare.py
@@ -45,8 +45,10 @@ def htrSummary(blocks=[], book=None, fedId=None,
         for otherData in block["otherData"].values():
             flavor(book, otherData, fedId)
 
-        for otherData in block["technicalData"].values():
-            flavor(book, otherData, fedId)
+        for techData in block["technicalData"].values():
+            # remove uHTR pad words from flavor histogram
+            if techData["technicalDataType"] or techData["channelId"] or techData["words"]:
+                flavor(book, techData, fedId)
 
         for triggerData in block["triggerData"].values():
             if "Flavor" in triggerData:
