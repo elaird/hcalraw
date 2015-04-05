3c658805f4745d11e9361ebf9994d907320f44d8
Edward Laird <elaird@gmail.com>
2015-04-05 17:42:18 +0200

    dont skip when mapping

--- analyze.py
+++ analyze.py
@@ -81,8 +81,9 @@ def eventMaps(chain, s={}):
              "skipWords64": s["skipWords64"],
              }
 
-    iEntry = s["nEventsSkip"]
-    while iEntry != s["nEventsMax"]:
+    iEntry = 0  # start from beginning, even when skipping events in the loop
+    nMapMax = 2 * s["nEventsMax"]  # a guess for how far to look not to miss out-of-order events
+    while iEntry != nMapMax:
         if chain.GetEntry(iEntry) <= 0:
             break
 
