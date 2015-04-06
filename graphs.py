fe31bc7764d7ca122f936e04e49d10df07f0c897
Edward Laird <elaird@gmail.com>
2015-04-07 00:36:13 +0200

    comment aux graphs

--- graphs.py
+++ graphs.py
@@ -616,11 +616,11 @@ def makeSummaryPdfMulti(inputFiles=[], feds1s=[], feds2s=[], pdf="summary.pdf"):
             pageTwo(f, feds1, feds2, canvas, pdf, names=["adc_vs_adc", "tp_vs_tp"])
             # pageTwo(f, feds1, feds2, canvas, pdf, names=["adc_vs_adc_both_soi"])
 
-        pageTwo(f, feds1, feds2, canvas, pdf, names=["frac0_vs_BcN_%d" % feds1[0]],
-                doYx=False, retitle=False)
+        # pageTwo(f, feds1, feds2, canvas, pdf, names=["frac0_vs_BcN_%d" % feds1[0]],
+        #         doYx=False, retitle=False)
 
         # pageThree(f, feds1, feds2, canvas, pdf, names=["frac0_vs_EvN"])
-        pageThree(f, feds1, feds2, canvas, pdf, names=["frac0_vs_time"])
+        # pageThree(f, feds1, feds2, canvas, pdf, names=["frac0_vs_time"])
 
         f.Close()
     canvas.Print(pdf + "]")
