531800d2042c273f89ed54e36d166af69c19c668
Edward Laird <elaird@gmail.com>
2015-07-14 01:44:35 +0200

    add run number

--- fiberId.sh
+++ fiberId.sh
@@ -3,8 +3,8 @@
 RUN=$1
 FILE=root://eoscms.cern.ch//store/group/dpg_hcal/comm_hcal/LS1/USC_${RUN}.root
 
-echo "VME:"
+echo "VME (run $RUN)"
 ./oneRun.py --file1=${FILE} --feds1=HCAL  --patterns | ./diff.py data/ref_vme_G.txt
 echo
-echo "uTCA:"
+echo "uTCA (run $RUN)"
 ./oneRun.py --file1=${FILE} --feds1=uHBEF --patterns | ./diff.py data/ref_utca_G.txt
