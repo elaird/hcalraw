baed9ab8284db73cd63685244bee0a5ba1d2976b
Edward Laird <elaird@gmail.com>
2015-07-14 15:12:44 +0200

    extract __book

--- autoBook.py
+++ autoBook.py
@@ -14,23 +14,21 @@ class autoBook(dict):
         self.fillOrder = []
         self.weight = 1
 
+
     @property
     def title(self):
         return self.__directory.GetName() if self.__directory else ""
 
+
     def fillGraph(self, x, name, title=""):
         self.fill(x, name, None, None, None, title=title)
 
-    def fill(self, x, name, N, low, up, w=None,
-             title="", xAxisLabels=[], yAxisLabels=[]):
 
-        tgraph = N is None
-        if w is None:
-            w = self.weight
-        if not name in self:
+    def __book(self, is_tgraph, x, name, N, low, up, title, xAxisLabels, yAxisLabels):
+        if True:
             self.__directory.cd()
             self.fillOrder.append(name)
-            if tgraph:
+            if is_tgraph:
                 self[name] = r.TGraph()
                 self[name].SetName(name)
                 self[name].SetTitle(title)
@@ -48,20 +46,29 @@ class autoBook(dict):
                                     N[1], low[1], up[1],
                                     N[2], low[2], up[2])
 
-            if (not tgraph) and not self[name].GetSumw2N():
+            if (not is_tgraph) and not self[name].GetSumw2N():
                 self[name].Sumw2()
             for i, label in enumerate(xAxisLabels):
                 self[name].GetXaxis().SetBinLabel(i+1, label)
             for i, label in enumerate(yAxisLabels):
                 self[name].GetYaxis().SetBinLabel(i+1, label)
 
-        if tgraph:
+
+    def fill(self, x, name, N, low, up, w=None,
+             title="", xAxisLabels=[], yAxisLabels=[]):
+
+        is_tgraph = N is None
+        if name not in self:
+            self.__book(is_tgraph, x, name, N, low, up, title, xAxisLabels, yAxisLabels)
+
+        if w is None:
+            w = self.weight
+
+        if is_tgraph:
             self[name].SetPoint(self[name].GetN(), x[0], x[1])
         elif type(x) != tuple:
             self[name].Fill(x, w)
-        elif type(N) != tuple:
-            self[name].Fill(x[0], x[1], w)
-        elif len(N) == 2:
+        elif len(N) == 2 or type(N) != tuple:
             self[name].Fill(x[0], x[1], w)
         else:
             self[name].Fill(x[0], x[1], x[2], w)
