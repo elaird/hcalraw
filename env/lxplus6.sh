621239dbd2b55c961b80b04844f5cf04c3a6f523
Edward Laird <elaird@gmail.com>
2016-04-06 10:44:02 +0200

    update cmssw

--- env/lxplus6.sh
+++ env/lxplus6.sh
@@ -1,6 +1,8 @@
-export SCRAM_ARCH=slc6_amd64_gcc491
-cd /afs/cern.ch/cms/${SCRAM_ARCH}/cms/cmssw/CMSSW_7_4_0/src
-eval `/afs/cern.ch/cms/common/scramv1 runtime -sh`
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+export SCRAM_ARCH=slc6_amd64_gcc493
+cd /cvmfs/cms.cern.ch/${SCRAM_ARCH}/cms/cmssw/CMSSW_8_0_3/src
+eval `scramv1 runtime -sh`
 cd - > /dev/null
 
 source /afs/cern.ch/project/eos/installation/cms/etc/setup.sh
