f5c19ea1f9ab5e6be00f26896782549b183e5981
Edward Laird <elaird@gmail.com>
2016-09-27 16:44:48 +0200

    accommodate directory move

--- fiberId.sh
+++ fiberId.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 RUN=$1
-FILE=root://eoscms.cern.ch//store/group/dpg_hcal/comm_hcal/USC/USC_${RUN}.root
+COMMON=root://eoscms.cern.ch//store/group/dpg_hcal/comm_hcal
+if [[ ( "$RUN" < 264261 ) ]] ; then
+    FILE=$COMMON/archival/20160914_USC_LS1_runs212958-264260/USC_${RUN}.root
+else
+    FILE=$COMMON/USC/USC_${RUN}.root
+fi
 
 echo "VME (run $RUN)"
 ./oneRun.py --file1=${FILE} --feds1=HCAL  --patterns | ./diff.py data/ref_vme_G.txt
