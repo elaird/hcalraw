d4e1c4352b5dced23a886d2a58b9813697cfe81a
Edward Laird <elaird@gmail.com>
2016-09-27 16:37:47 +0200

    dont export EOS

--- env/lxplus6.sh
+++ env/lxplus6.sh
@@ -6,6 +6,6 @@ eval `scramv1 runtime -sh`
 cd - > /dev/null
 
 source /afs/cern.ch/project/eos/installation/cms/etc/setup.sh
-export EOS=root://eoscms.cern.ch//store/group/dpg_hcal/comm_hcal/
+EOS=root://eoscms.cern.ch//store/group/dpg_hcal/comm_hcal/
 export LS1=$EOS/archival/20160914_USC_LS1_runs212958-264260/
 export USC=$EOS/USC/
