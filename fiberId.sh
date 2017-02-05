2c054656dc9c1dc3217f42e8b4f70481f4db8272
Edward Laird <elaird@gmail.com>
2017-02-05 20:29:17 +0100

    accommodate new eos dir

--- fiberId.sh
+++ fiberId.sh
@@ -4,8 +4,10 @@ RUN=$1
 COMMON=root://eoscms.cern.ch//store/group/dpg_hcal/comm_hcal
 if [[ ( "$RUN" < 264261 ) ]] ; then
     FILE=$COMMON/archival/20160914_USC_LS1_runs212958-264260/USC_${RUN}.root
-elif [[ ( "$RUN" < 99999999 ) ]] ; then
+elif [[ ( "$RUN" < 280486 ) ]] ; then
     FILE=$COMMON/USC/USC_${RUN}.root
+elif [[ ( "$RUN" < 99999999 ) ]] ; then
+    FILE=$COMMON/USC/run${RUN}/USC_${RUN}.root
 else
     FILE=$1
 fi
