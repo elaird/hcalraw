4e69422062ebc8cadd8545ad6b562020ca77dc1d
Edward Laird <elaird@gmail.com>
2017-04-01 09:42:06 +0200

    rework color

--- printer.py
+++ printer.py
@@ -1,46 +1,56 @@
 # https://wiki.archlinux.org/index.php/Color_Bash_Prompt
+# http://misc.flogisoft.com/bash/tip_colors_and_formatting
 
 __color = True  # overwritten by oneRun.py
 
-def __line(f='', s=''):
+def __line(f, s, p):
     if __color:
-        print '\033['+ f + s + '\033[0m'
-    else:
+        s = '\033['+ f + s + '\033[0m'
+
+    if p:
         print s
+    return s
 
 
 def error(s=''):
-    __line('91m', 'ERROR: '+s)
-
+    return red('ERROR: ' + s, True)
 
 def warning(s=''):
-    __line('91m', 'WARNING: '+s)
-
+    return red('WARNING: ' + s, True)
 
 def info(s=''):
-    __line('0m', 'INFO: '+s)
-
+    return msg('INFO: ' + s)
 
 def msg(s=''):
-    __line('0m', s)
+    return __line('0m', s, True)
 
 
-def green(s=''):
-    __line('32m', s)
+def bold(s='', p=True):
+    return __line('1m', s, p)
 
+def underline(s='', p=True):
+    return __line('4m', s, p)
 
-def yellow(s=''):
-    __line('33m', s)
+def green(s='', p=True):
+    return __line('32m', s, p)
 
+def yellow(s='', p=True):
+    return __line('33m', s, p)
 
-def blue(s=''):
-    # __line('34m', s)
-    __line('94m', s)
+def dark_blue(s='', p=True):
+    return __line('34m', s, p)
 
+def purple(s='', p=True):
+    return __line('35m', s, p)
 
-def purple(s=''):
-    __line('35m', s)
+def cyan(s='', p=True):
+    return __line('36m', s, p)
 
+def gray(s, p=True):
+    return __line('90m', s, p)
 
-def cyan(s=''):
-    __line('36m', s)
+def red(s, p=True):
+    return __line('91m', s, p)
+
+def blue(s='', p=True):
+    return __line('94m', s, p)
