c79c75b0d7c31c7bece1208682154c98c85bb6c2
Edward Laird <elaird@gmail.com>
2017-04-08 19:02:06 +0200

    leverage look.py

--- fiberId.sh
+++ fiberId.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-RUN=$1
-COMMON=root://eoscms.cern.ch//store/group/dpg_hcal/comm_hcal
-if [[ ( "$RUN" < 264261 ) ]] ; then
-    FILE=$COMMON/archival/20160914_USC_LS1_runs212958-264260/USC_${RUN}.root
-elif [[ ( "$RUN" < 280486 ) ]] ; then
-    FILE=$COMMON/USC/USC_${RUN}.root
-elif [[ ( "$RUN" < 99999999 ) ]] ; then
-    FILE=$COMMON/USC/run${RUN}/USC_${RUN}.root
-else
-    FILE=$1
-fi
-
-./oneRun.py --file1=${FILE} --plugins=patterns ${@:2} | ./diff.py data/ref_2017.txt
+./look.py $1 --quiet --plugins=patterns ${@:2} | ./diff.py data/ref_2017.txt
