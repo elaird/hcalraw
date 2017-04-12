0e9dc9cb3b018b04c17c02b401c04cc8968b7abf
Edward Laird <elaird@gmail.com>
2017-04-12 11:07:31 +0200

    remove old setup files

--- env/slc5-cmssw.sh
+++
@@ -1,6 +0,0 @@
-#note that CASTOR will work only with some CMSSW versions
-#see https://cern.service-now.com/service-portal/article.do?n=KB0001585
-
-cd /afs/cern.ch/cms/slc5_amd64_gcc462/cms/cmssw/CMSSW_5_3_7/src
-eval `scramv1 runtime -sh`
-cd - > /dev/null
--- env/slc6-root6.sh
+++
@@ -1,3 +0,0 @@
-# http://root.cern.ch/drupal/content/production-version-602
-source /afs/cern.ch/sw/lcg/external/gcc/4.8/x86_64-slc6/setup.sh
-source /afs/cern.ch/sw/lcg/app/releases/ROOT/6.02.00/x86_64-slc6-gcc48-opt/root/bin/thisroot.sh
