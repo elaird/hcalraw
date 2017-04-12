c3c19012b6b0fe52428d7cf155bcd2e19621e7ed
Edward Laird <elaird@gmail.com>
2017-04-12 11:07:48 +0200

    add a setup file

---
+++ env/cc7-cvmfs.sh
@@ -0,0 +1,3 @@
+# https://root.cern.ch/content/release-60806
+. /cvmfs/sft.cern.ch/lcg/contrib/gcc/4.8/x86_64-centos7-gcc48-opt/setup.sh
+. /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.08.06/x86_64-centos7-gcc48-opt/root/bin/thisroot.sh
