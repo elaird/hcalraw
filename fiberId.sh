19ba3067a2915ae204bfeb07f65632b957eb7917
Edward Laird <elaird@gmail.com>
2017-04-22 15:15:40 +0200

    simplify handling of arguments

--- fiberId.sh
+++ fiberId.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./look.py $1 --quiet --plugins=patterns --nevents=1 ${@:2} | ./diff.py data/ref_2017.txt
+./look.py --quiet --plugins=patterns --nevents=1 $* | ./diff.py data/ref_2017.txt
