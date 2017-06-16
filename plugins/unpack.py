6e8919060aac678334925f5f8a86a93ea9068b34
Edward Laird <elaird@gmail.com>
2017-06-16 13:03:11 +0200

    extract functions into plugin

---
+++ plugins/unpack.py
@@ -0,0 +1,140 @@
+import decode
+import printer
+import struct
+
+
+def collected(tree=None, specs={}):
+    raw = {}
+    kargs = {}
+    for item in ["dump", "unpack", "lastNAmcs", "nBytesPer", "skipWords64"]:
+        kargs[item] = specs[item]
+
+    for fedId, wargs in sorted(specs["wargs"].iteritems()):
+        raw[fedId] = unpacked(fedData=specs["wfunc"](**wargs),
+                              warn=specs["warnUnpack"],
+                              **kargs)
+
+    raw[None] = {"iEntry": tree.GetReadEntry()}
+    for key in ["label", "dump", "crateslots", "firstNTs", "perTs"]:
+        raw[None][key] = specs[key]
+
+    return raw
+
+
+def w64(fedData, jWord64, nBytesPer):
+    if nBytesPer == 1:
+        offset = 8*jWord64
+        bytes = [fedData.at(offset+iByte) for iByte in range(8)]
+        word64 = struct.unpack('Q', "".join(bytes))[0]
+        #like above with 'B'*8 rather than 'Q':
+        #b = [ord(fedData.at(offset+iByte)) for iByte in range(8)]
+    elif nBytesPer == 4:
+        word64 = fedData.at(2*jWord64)
+        word64 += fedData.at(2*jWord64 + 1) << 32
+    elif nBytesPer == 8:
+        word64 = fedData.at(jWord64)
+    return word64
+
+
+def unpackedHeader(spec):
+    wargs = spec["wargs"][spec["fedId0"]]
+    return unpacked(fedData=spec["wfunc"](**wargs),
+                    nBytesPer=spec["nBytesPer"],
+                    skipWords64=spec["skipWords64"],
+                    headerOnly=True)
+
+
+# for format documentation, see decode.py
+def unpacked(fedData=None, nBytesPer=None, headerOnly=False, unpack=True,
+             warn=True, skipWords64=[], dump=-99, lastNAmcs=0):
+    assert fedData
+    assert nBytesPer in [1, 4, 8], "ERROR: invalid nBytes per index (%s)." % str(nBytesPer)
+
+    header = {"iWordPayload0": 6,
+              "utca": None,
+              }  # modified by decode.header
+    trailer = {}
+    other = {}
+    htrBlocks = {}
+
+    nWord64Trailer = 1
+
+    nWord64 = fedData.size()*nBytesPer/8
+    nWord16Skipped = 0
+
+    nToSkip = len(set(skipWords64))
+    skipped64 = []
+
+    for jWord64 in range(nWord64):
+        if not unpack:
+            continue
+
+        word64 = w64(fedData, jWord64, nBytesPer)
+
+        if jWord64 in skipWords64:
+            skipped64.append(word64)
+            continue
+
+        iWord64 = jWord64 - len(skipped64)
+
+        if 12 <= dump:
+            if not iWord64:
+                print "#iw64 w64"
+            print "%5d" % iWord64, "%016x" % word64
+
+        if iWord64 < header["iWordPayload0"]:
+            decode.header(header, iWord64, word64, lastNAmcs)
+            if header.get("uFoV"):
+                nWord64Trailer = 2  # accommodate block trailer
+            iWordTrailer0 = nWord64 - nToSkip - nWord64Trailer
+        elif headerOnly:
+            break
+        elif lastNAmcs and iWord64 < header["iWordPayloadn"]:
+            continue
+        elif iWord64 < iWordTrailer0:
+            for i in range(4):
+                word16 = (word64 >> (16*i)) & 0xffff
+                iWord16 = 4*iWord64+i
+                returnCode = decode.payload(htrBlocks,
+                                            iWord16=iWord16,
+                                            word16=word16,
+                                            word16Counts=header["word16Counts"],
+                                            utca=header["utca"],
+                                            fedId=header["FEDid"],
+                                            warn=warn,
+                                            dump=dump)
+                if returnCode is None:
+                    continue
+
+                # ignore VME pad words (zero)
+                if not header["utca"] and iWord64 + 1 == iWordTrailer0:
+                    if 4 * header["iWordPayload0"] + sum(header["word16Counts"]) <= iWord16:
+                        if not word16:
+                            continue
+
+                nWord16Skipped += 1
+                if warn:
+                    printer.warning(" ".join(["skipping",
+                                              "FED %d" % header["FEDid"],
+                                              "event %d" % header["EvN"],
+                                              "iWord16 %d" % iWord16,
+                                              "word16 0x%04x" % word16,
+                                              ]))
+        else:
+            if "htrIndex" in htrBlocks:
+                del htrBlocks["htrIndex"]  # fixme
+
+            if header["uFoV"] and (iWord64 == nWord64 - nToSkip - 2):
+                decode.block_trailer_ufov1(trailer, iWord64, word64)
+            else:
+                decode.trailer(trailer, iWord64, word64)
+
+    decode.other(other, skipped64)
+
+    return {"header": header,
+            "trailer": trailer,
+            "htrBlocks": htrBlocks,
+            "other": other,
+            "nBytesSW": 8*nWord64,
+            "nWord16Skipped": nWord16Skipped,
+            }
