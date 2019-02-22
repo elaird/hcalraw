23ec99d4d14c467ff88fea56fe4f1138a2b7d4d7
Edward Laird <elaird@gmail.com>
2019-02-22 15:41:51 +0100

    remove old file

--- env/slc6-pypy.sh
+++
@@ -1,6 +0,0 @@
-# http://root.cern.ch/drupal/content/pypyroot
-source /afs/.cern.ch/sw/lcg/external/pypy/x86_64-slc6/setup-pypyroot.sh
-mkdir bin >& /dev/null
-rm bin/python >& /dev/null
-ln -s `which pypyroot` bin/python >& /dev/null
-export PATH=bin/:${PATH}
