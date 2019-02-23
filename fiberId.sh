3c817a6f076cf7c827bcf4222620b674572c4fcc
Edward Laird <elaird@gmail.com>
2019-02-23 14:12:35 +0100

    use 2019 ref

--- fiberId.sh
+++ fiberId.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./look.py --quiet --dump=0 --plugins=unpack,patterns --nevents=1 $* | ./diff.py data/ref_2018.txt
+./look.py --quiet --dump=0 --plugins=unpack,patterns --nevents=1 $* | ./diff.py data/ref_2019.txt
