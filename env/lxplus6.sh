9c67960272330f87881bdd16a2055fc2f825ea55
Edward Laird <elaird@gmail.com>
2019-07-19 17:17:47 +0200

    update release

--- env/lxplus6.sh
+++ env/lxplus6.sh
@@ -1,7 +1,7 @@
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 
-export SCRAM_ARCH=slc6_amd64_gcc630
-cd /cvmfs/cms.cern.ch/${SCRAM_ARCH}/cms/cmssw/CMSSW_10_0_4/src
+export SCRAM_ARCH=slc7_amd64_gcc630
+cd /cvmfs/cms.cern.ch/${SCRAM_ARCH}/cms/cmssw/CMSSW_10_2_0/src
 eval `scramv1 runtime -sh`
 cd - > /dev/null
 
