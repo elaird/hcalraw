1ab7f056ce1a46b258320e5e92636deb6c786bf6
Edward Laird <elaird@gmail.com>
2019-12-12 14:58:15 +0100

    update to era N

--- make_fiberid_references.py
+++ make_fiberid_references.py
@@ -37,7 +37,7 @@ def B904():
             print("%su%2d %02d %02d: %s %1d %1d" % (lineStart, crate, slot, uhtr_fib, rbx, rm, rm_fib))
 
 
-def loop(filenames=[], nExpected=None, iCrate=None, iUhtr=None, iUhtrFib=None, iRbx=None, iRm=None, iRmFib=None, vme=False, fedOffset=0):
+def loop(filenames=[], nExpected=None, iCrate=None, iUhtr=None, iUhtrFib=None, iRbx=None, iRm=None, iRmFib=None):
     out = []
     for filename in filenames:
         f = open(filename)
@@ -55,11 +55,7 @@ def loop(filenames=[], nExpected=None, iCrate=None, iUhtr=None, iUhtrFib=None, i
                 print(len(fields), fields)
                 continue
 
-            if vme:
-                be = "%3s %02d %02d" % (int(fields[iCrate]) + fedOffset, int(fields[iUhtr]), int(fields[iUhtrFib]))  # fedId spigot fiber
-            else:
-                be = "u%2d %02d %02d" % (int(fields[iCrate]), int(fields[iUhtr]), int(fields[iUhtrFib]))
-
+            be = "u%2d %02d %02d" % (int(fields[iCrate]), int(fields[iUhtr]), int(fields[iUhtrFib]))
             fe = "%s %1d %1d" % (fields[iRbx], int(fields[iRm]), int(fields[iRmFib]))
             out.append("%s%s: %s" % (lineStart, be, fe))
         f.close()
@@ -78,45 +74,49 @@ def check_for_duplicates(l):
 
 
 def HB():
-    return loop(["Lmap_ngHB_M_20190223.txt"], nExpected=27,
+    return loop(["Lmap_ngHB_N_20191210.txt"], nExpected=27,
                 iCrate=-7, iUhtr=-6, iUhtrFib=-5, iRbx=6, iRm=12, iRmFib=13)
 
 
 def HBcalib():
-    return loop(["Lmap_ngHBcalib_M_20190416.txt"], nExpected=31,
+    return loop(["Lmap_ngHBcalib_N_20191210.txt"], nExpected=31,
                 iCrate=-5, iUhtr=-4, iUhtrFib=-3, iRbx=6, iRm=11, iRmFib=12)
 
 
 def HE():
-    return loop(["Lmap_ngHE_M_20190223.txt"], nExpected=26,
-                iCrate=19, iUhtr=20, iUhtrFib=21, iRbx=6, iRm=11, iRmFib=12)
+    return loop(["Lmap_ngHE_N_20191210.txt"], nExpected=27,
+                iCrate=-7, iUhtr=-6, iUhtrFib=-5, iRbx=6, iRm=12, iRmFib=13)
 
 
 def HEcalib():
-    return loop(["Lmap_ngHEcalib_M_20190129.txt"], nExpected=29,
-                iCrate=24, iUhtr=25, iUhtrFib=26, iRbx=6, iRm=10, iRmFib=11)
+    return loop(["Lmap_ngHEcalib_N_20191210.txt"], nExpected=31,
+                iCrate=26, iUhtr=27, iUhtrFib=28, iRbx=6, iRm=11, iRmFib=12)
 
 
 def HF():
-    return loop(["Lmap_ngHF_M_20190129.txt"], nExpected=37,
+    return loop(["Lmap_ngHF_N_20191210.txt"], nExpected=36,
                 iCrate=27, iUhtr=28, iUhtrFib=30, iRbx=6, iRm=19, iRmFib=22)
 
 
 def HFcalib():
-    return loop(["Lmap_ngHFcalib_M_20190129.txt"], nExpected=24,
-                iCrate=-7, iUhtr=-6, iUhtrFib=-5, iRbx=6, iRm=8, iRmFib=10)
+    return loop(["Lmap_ngHFcalib_N_20191210.txt"], nExpected=23,
+                iCrate=-6, iUhtr=-5, iUhtrFib=-4, iRbx=6, iRm=8, iRmFib=10)
 
 
 def HO():
-    return loop(["ngHO_Lmap_allCates_alligned.txt"], nExpected=22,
+    return loop(["Lmap_ngHO_N_20191210.txt"], nExpected=22,  # deleting the final four columns from most rows
+                iCrate=16, iUhtr=17, iUhtrFib=19, iRbx=6, iRm=12, iRmFib=13)
+
+
+def HOcalib():
+    return loop(["Lmap_ngHOcalib_N_20191210.txt"], nExpected=22,
                 iCrate=16, iUhtr=17, iUhtrFib=19, iRbx=6, iRm=12, iRmFib=13)
 
 
 def USC():
-    # see http://cmsdoc.cern.ch/cms/HCALdocs/document/Mapping/Yuan/2019-jan-29/Lmap/
-    # and http://cmsdoc.cern.ch/cms/HCALdocs/document/Mapping/Yuan/2019-apr-16/Lmap/
-    # and https://desycloud.desy.de/index.php/s/mCg4JeYp6XpisCo
-    l = HB() + HBcalib() + HE() + HEcalib() + HF() + HO() # + HFcalib()
+    # see https://hypernews.cern.ch/HyperNews/CMS/get/hcal-performance/1609.html
+    # and http://cmsdoc.cern.ch/cms/HCALdocs/document/Mapping/Yuan/2019-dec-10/Lmap/
+    l = HB() + HBcalib() + HE() + HEcalib() + HF() + HFcalib() + HO() + HOcalib()
     l = set(l)
     check_for_duplicates(l)
     for line in sorted(l):
